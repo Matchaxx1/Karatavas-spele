c15e7a2a1229a68303d20ffc7bd652cce48d9cc5
Matīss Stepuļenoks <matiss03082@gmail.com>
2024-12-08 00:19:58 +0200

    .

--- Karatavas/Karatavas/Form2.Designer.cs
+++ Karatavas/Karatavas/Form2.Designer.cs
@@ -366,7 +366,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
-            this.label2.Location = new System.Drawing.Point(12, 166);
+            this.label2.Location = new System.Drawing.Point(24, 167);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(319, 38);
             this.label2.TabIndex = 4;
@@ -375,7 +375,7 @@
             // butMinet
             // 
             this.butMinet.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butMinet.Location = new System.Drawing.Point(19, 207);
+            this.butMinet.Location = new System.Drawing.Point(31, 208);
             this.butMinet.Name = "butMinet";
             this.butMinet.Size = new System.Drawing.Size(105, 41);
             this.butMinet.TabIndex = 5;
@@ -387,7 +387,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(14, 251);
+            this.label3.Location = new System.Drawing.Point(26, 252);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(239, 28);
             this.label3.TabIndex = 6;
@@ -397,7 +397,7 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(243, 251);
+            this.label5.Location = new System.Drawing.Point(255, 252);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(24, 28);
             this.label5.TabIndex = 17;
@@ -418,7 +418,7 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(314, 166);
+            this.label6.Location = new System.Drawing.Point(326, 167);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(79, 38);
             this.label6.TabIndex = 26;
@@ -427,7 +427,7 @@
             // butMinetVardu
             // 
             this.butMinetVardu.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butMinetVardu.Location = new System.Drawing.Point(19, 298);
+            this.butMinetVardu.Location = new System.Drawing.Point(31, 299);
             this.butMinetVardu.Name = "butMinetVardu";
             this.butMinetVardu.Size = new System.Drawing.Size(163, 41);
             this.butMinetVardu.TabIndex = 27;
@@ -439,7 +439,7 @@
             // 
             this.textBox1.Cursor = System.Windows.Forms.Cursors.PanNW;
             this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 10F);
-            this.textBox1.Location = new System.Drawing.Point(19, 345);
+            this.textBox1.Location = new System.Drawing.Point(31, 346);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(204, 26);
             this.textBox1.TabIndex = 28;
